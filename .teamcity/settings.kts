ee06512dbbaef313813498d5f3f2950cc6d1c7da
Carlolong <89375472+AsT2U@users.noreply.github.com>
2024-04-07 23:07:58 +0800

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -27,7 +27,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2023.05"
+version = "2023.03"
 
 project {
     buildType(Build)
